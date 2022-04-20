cdc04c0240038427b98a46ebedaffdaebfe4192f
Zalan Meggyesi <zmeggyesi@skawa.hu>
2022-04-20 09:15:35 +0200

    chore: update with latest working status

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "cf_domain" {
-  value = aws_cloudfront_distribution.translations_at_root[*].domain_name
+//  value = aws_cloudfront_distribution.translations_at_root[*].domain_name
+  value = split("," , trim("%{ for distro in aws_cloudfront_distribution.translations_at_root[*] }%{ for alias in distro.aliases[*]}${alias} 300 IN CNAME ${distro.domain_name},%{ endfor }%{ endfor }", ","))
 }
--- variables.tf
+++ variables.tf
@@ -1,19 +1,28 @@
-variable "locales" {
-  description = "List of published language objects. The two attributes are `domain` for the target language domain, and `locale` for the locale code. MUST be given as list of two-by-two letter locales."
-  type        = list(object({
-    domain = string
-    language = string
+variable "domain_to_locale" {
+  description = "List of published language objects. The two attributes are `target` for the target language domain, and `locale` for the locale code. MUST be given as list of two-by-two letter locales."
+  type = list(object({
+    target = string
+    locale = string
   }))
   default = []
 }
 variable "source_domain" {
-  description = "The original domain of the site"
-  type = string
-  default = ""
+  description = "The original domain of the site. Used to create the route for the root and set configuration during subdirectory publishing"
+  type        = string
+  default     = ""
+}
+variable "prefix_to_locale" {
+  description = "List of published language objects. The two attributes are `target` for the path prefix, and `locale` for the locale code. MUST be given as list of two-by-two letter locales."
+  default     = []
+  type = list(object({
+    target = string
+    locale = string
+    origin = bool
+  }))
 }
 
 variable "project" {
-  description = "The project ID"
+  description = "The project ID provided by the LSP"
   type        = string
 }
 variable "app_domain" {
@@ -22,5 +31,5 @@ variable "app_domain" {
 }
 variable "acm_cert_arn" {
   description = "ARN of the dynamic certificate provisioned by AWS. Can be left empty, in which case HTTPS will not work!"
-  default = ""
+  default     = ""
 }
